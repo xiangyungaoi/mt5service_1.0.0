af4f50d24413fad75d4805c34c0ed1d22cedce29
gaoyx <gaoyx@cict.com>
2019-05-23 11:14:42 +0800

    Initial commit

---
+++ src/main/java/com/caetp/mt5service/Websocket2mt53Application.java
@@ -0,0 +1,13 @@
+package com.caetp.mt5service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Websocket2mt53Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Websocket2mt53Application.class, args);
+	}
+
+}
